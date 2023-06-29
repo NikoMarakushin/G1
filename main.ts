dda5fe8236709e1b8d8a617d00f8ccc404b4562d
Niko <126806658+NikoMarakushin@users.noreply.github.com>
2023-06-29 19:08:04 +0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<script>
+"use strict";
+
+["Бильбо", "Гэндальф", "Назгул"].forEach((item, index, array) => {
+  alert(`У ${item} индекс ${index} в ${array}`);
+});
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
